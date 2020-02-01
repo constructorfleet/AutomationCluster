65f3748a570df612280fc49a6c734dfb66e64c71
Teagan glenn <teagan.m.glenn@gmail.com>
2020-01-31 22:27:57 -0700

    Fix config path

--- run_mesh_instances.sh
+++ run_mesh_instances.sh
@@ -3,7 +3,7 @@ sudo docker run -d \
 	--name=automation_zwave \
 	--network=automation-cluster \
 	-v /etc/localtime:/etc/localtime:ro \
-	-v ./automation/zwave:/config \
+	-v ./configs/zwave:/config \
 	--device /dev/ttyACM0:/dev/ttyACM0 \
 	--privileged \
 	homeassistant/home-assistant:0.100.3
