24a1bcba8f20c2672fa57a96af663f8666469af6
Teagan glenn <teagan.m.glenn@gmail.com>
2020-03-01 01:06:28 -0700

    One compose file

--- run_mesh_instances.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-sudo docker run -d \
-	--name=automation_zwave \
-	--network=automation-cluster \
-	-v /etc/localtime:/etc/localtime:ro \
-	-v ./configs/zwave:/config \
-	--device /dev/ttyACM0:/dev/ttyACM0 \
-	--privileged \
-	homeassistant/home-assistant:0.100.3
