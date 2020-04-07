9987890c9adee583c70a5000eb9ec94309b3a0cd
Teagan glenn <teagan.m.glenn@gmail.com>
2020-04-07 10:47:13 -0600

    different wait

--- docker-up.sh
+++ docker-up.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
 docker-compose -f docker-compose.core.yml up -d
-while ! echo exit | nc localhost 18830; do sleep 10; done
+
+while ! telnet localhost 1883
+do
+  echo "$(date) - still trying"
+  sleep 1
+done
+echo "$(date) - connected successfully"
 
 echo "Core up"
