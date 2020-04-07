58a01db4c7e9723eee835384a3ce5e05b4be580c
Teagan glenn <teagan.m.glenn@gmail.com>
2020-04-07 11:09:56 -0600

    up/down

---
+++ docker-down.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+docker-compose -f docker-compose.services.yml down
+docker-compose -f docker-compose.core.yml down
+
--- docker-up.sh
+++ docker-up.sh
@@ -8,3 +8,10 @@ echo "Core up"
 sleep 10
 
 docker-compose -f docker-compose.services.yml up -d
+echo "Services up"
+
+while ! nc -z localhost 8123 </dev/null; do sleep 10; done
+
+docker exec -it automation_master apk add openldap-clients
+
+echo "Complete"
