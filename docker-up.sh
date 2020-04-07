8106703246d6a9efea8115b9839f871093e4fdad
Teagan glenn <teagan.m.glenn@gmail.com>
2020-04-07 10:54:09 -0600

    Bring up services

--- docker-up.sh
+++ docker-up.sh
@@ -5,3 +5,6 @@ docker-compose -f docker-compose.core.yml up -d
 while ! nc -z localhost 1883 </dev/null; do sleep 10; done
 
 echo "Core up"
+sleep 10
+
+docker-compose -f docker-compose.services.yml up -d
