c3c246eacbdeceb752fb98df02158f093ccfcd97
Teagan glenn <teagan.m.glenn@gmail.com>
2020-04-09 14:58:12 -0600

    update script

--- docker-down.sh
+++ docker-down.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 docker-compose -f docker-compose.services.yml down
+docker-compose -f docker-compose.configs.yml down
 docker-compose -f docker-compose.core.yml down
-docker-compose -f docker-compose.services.yml down
 
